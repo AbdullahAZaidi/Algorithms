29c5524d451ea83388c51ee80ce5ef36c9a3cbc3
AbdullahAZaidi <66746291+AbdullahAZaidi@users.noreply.github.com>
2020-06-10 12:44:23 -0700

    Create InsertionSort.cpp

---
+++ InsertionSort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+
+vector<int> bubbleSort (vector<int> r){
+    int count = 0;
+    vector <int> w;
+    for (int i= 0; i < r.size(); i++){
+        for (int j= 0; j < r.size(); j++){
+            if (r[i]<r[j]){
+                int temp = r[i];
+                r[i] = r[j];
+                r[j] = temp;
+            }
+        }
+                
+    }
+    return r;
+}
+
+
+int main()
+{
+    vector<int> v = {1,2,5,5,6,7,3,4,4,3,343,565,77};
+    vector<int> p = bubbleSort(v);
+    for (int i= 0; i < p.size(); i++){
+         cout << p[i] <<endl;
+    }
+   
+    return 0;
+}
