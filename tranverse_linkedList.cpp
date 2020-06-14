62e89208cdd05dd51a21cc0e5fe5d09b6aad2251
AbdullahAZaidi <66746291+AbdullahAZaidi@users.noreply.github.com>
2020-06-14 11:36:26 -0700

    Create tranverse_linkedList.cpp

---
+++ tranverse_linkedList.cpp
@@ -0,0 +1,40 @@
+/******************************************************************************
+Tranverse through linked list
+
+*******************************************************************************/
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+int main () { 
+    Node* head = NULL;
+    Node* second = NULL;
+    Node* third = NULL;
+    
+    head = new Node();
+    second = new Node();
+    third = new Node();
+    
+    
+    head->data = 1;
+    head->next = second; 
+    
+    second->data = 2;
+    second->next = third;
+    
+    third->data = 3;
+    third->next = NULL;
+    
+    while(head!=NULL){
+        cout << head->data << endl;
+        head =head->next;
+    }
+}
+ 
