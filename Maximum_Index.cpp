0de965ebfbcf828fd0bb1868ad8cec7337fedfe9
AbdullahAZaidi <66746291+AbdullahAZaidi@users.noreply.github.com>
2020-06-13 22:37:03 -0700

    Update Maximum_Index.cpp

--- Maximum_Index.cpp
+++ Maximum_Index.cpp
@@ -1,6 +1,6 @@
-//****
+/****
 Given an array A[] of N positive integers. The task is to find the maximum of j - i subjected to the constraint of A[i] <= A[j].
-*****//
+*****/
 
 
 #include <stdio.h>
