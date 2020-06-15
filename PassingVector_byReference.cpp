6b8ac723b1ac6b31f6a3845da6e0c298f5b963fe
AbdullahAZaidi <66746291+AbdullahAZaidi@users.noreply.github.com>
2020-06-15 00:21:12 -0700

    Update PassingVector_byReference.cpp

--- PassingVector_byReference.cpp
+++ PassingVector_byReference.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 vector<int> AddOne(vector<int> &j)
 {
-    // Dereference the pointer and increment the int being pointed to.
+
     for (int i =0; i < j.size(); i++){
         j[i]+=1;
     }
@@ -20,8 +20,7 @@ int main()
 {
     vector<int> v = {1,2,3,4,5};
     
-    // Declare a pointer to i:
-    //int* pi = &i;
+
     v = AddOne(v);
     for (int i =0; i < v.size(); i++)
     {
